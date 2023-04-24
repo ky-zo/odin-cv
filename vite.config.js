9f2398fb69a096ef33200e720375489ec41c0faa
ky-zo <zowczak.kamil@gmail.com>
2023-04-24 19:51:19 +0700

    ?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/ky-zo/", // Replace with your GitHub repo name
+  build: {
+    outDir: "dist",
+  },
+});
