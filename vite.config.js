196fa70c6ef3ce992e49bd2ca43ee580dd858582
ky-zo <zowczak.kamil@gmail.com>
2023-04-24 19:58:12 +0700

    homepage fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ky-zo/", // Replace with your GitHub repo name
+  base: "/odin-cv/", // Replace with your GitHub repo name
   build: {
     outDir: "dist",
   },
